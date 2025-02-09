f2249736fa7cb24e1ae627b712920dd719369d5c
15Mithup l <mithunkumarlagumaiya@gmail.com>
2025-02-09 17:53:18 +0000

    chore: Optimize database query with indexes

--- element.sh
+++ element.sh
@@ -10,3 +10,11 @@ then
 fi
 
 QUERY_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number::TEXT='$1' OR symbol='$1' OR name='$1'")
+
+if [[ -z $QUERY_RESULT ]]
+then
+  echo "I could not find that element in the database."
+else
+  IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$QUERY_RESULT"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+fi
